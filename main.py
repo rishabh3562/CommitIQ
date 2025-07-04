60ae8718a3fe7b2b2b674e3d143c07f73bfcf851
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2025-07-04 23:59:38 +0530

    feat: socket connection with slack

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 from dotenv import load_dotenv
 load_dotenv(override=True)
 
+from bot.slack_bot import app, SLACK_APP_TOKEN
 
 if __name__ == "__main__":
-    print("working fine")
-
+    from slack_bolt.adapter.socket_mode import SocketModeHandler
+    print("Starting Slack bot...")
+    SocketModeHandler(app, SLACK_APP_TOKEN).start()
